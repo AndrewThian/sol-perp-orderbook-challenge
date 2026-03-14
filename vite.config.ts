2b6f997db9f407dbe6c9bb7b81d7ddd9d7f68d7a
AndrewThian <andrewbobbythian@gmail.com>
2026-03-14 16:00:24 +0800

    enforce test structure

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './tests/setup.ts',
+    setupFiles: './src/test-setup.ts',
+    include: ['src/**/__tests__/**/*.spec.ts'],
   },
 })
