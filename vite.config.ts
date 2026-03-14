5e1ccd579d78c79053857b14a4211351bd200f09
AndrewThian <andrewbobbythian@gmail.com>
2026-03-14 16:06:25 +0800

    update vite test and tsconfig to accept alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@/assets': path.resolve(__dirname, 'src/assets'),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/lib': path.resolve(__dirname, 'src/lib'),
+      '@/schemas': path.resolve(__dirname, 'src/schemas'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
